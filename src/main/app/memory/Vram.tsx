eeb9a7c682c5469a41aa920b37bf3fd0ab294977
hkamran490 <hkamran490@gmail.com>
2019-12-28 02:02:19 -0500

    -clearing default value.

--- src/main/app/memory/Vram.tsx
+++ src/main/app/memory/Vram.tsx
@@ -10,43 +10,6 @@ export class Vram {
     constructor() {
         this.data = new Array(Vram.size);
         this.data.fill(0, 0, Vram.size);
-
-        this.data[0] = 0x7C;
-        this.data[1] = 0x7C;
-        this.data[2] = 0x82;
-        this.data[3] = 0xEE;
-        this.data[4] = 0x82;
-        this.data[5] = 0xFE;
-        this.data[6] = 0x7C;
-        this.data[7] = 0x7C;
-
-        this.data[8] = 0x00;
-        this.data[9] = 0x00;
-        this.data[10] = 0xD6;
-        this.data[11] = 0x82;
-        this.data[12] = 0x54;
-        this.data[13] = 0x44;
-        this.data[14] = 0x38;
-        this.data[15] = 0x38;
-
-        this.data[16] = 0x7C;
-        this.data[17] = 0x00;
-        this.data[18] = 0xAA;
-        this.data[19] = 0x10;
-        this.data[20] = 0x82;
-        this.data[21] = 0x00;
-        this.data[22] = 0x7C;
-        this.data[23] = 0x00;
-
-        this.data[24] = 0x10;
-        this.data[25] = 0x00;
-        this.data[26] = 0x54;
-        this.data[27] = 0x00;
-        this.data[28] = 0x38;
-        this.data[29] = 0x00;
-        this.data[30] = 0x00;
-        this.data[31] = 0x00;
-
     }
 
     public readByte(address: Address): number {
