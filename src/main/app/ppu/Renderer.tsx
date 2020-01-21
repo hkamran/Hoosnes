ea90d8d5890bbe93ddae8e082a086b32fdc2e84c
hkamran490 <hkamran490@gmail.com>
2020-01-20 21:01:59 -0500

    initial impl of sprite rendering.

--- src/main/app/ppu/Renderer.tsx
+++ src/main/app/ppu/Renderer.tsx
@@ -1,10 +1,11 @@
 import {Ppu} from "./Ppu";
 import {Bit} from "../util/Bit";
-import {Color} from "./Palette";
+import {BppType, Color} from "./Palette";
 import {Screen, ScreenRegion} from "./Screen";
 import {Dimension, Tile, TileAttributes} from "./Tiles";
 import {TileMap} from "./TileMaps";
 import {Address} from "../bus/Address";
+import {Sprite} from "./Sprites";
 
 export class Renderer {
 
@@ -41,6 +42,35 @@ export class Renderer {
             if (color.opacity == 0) color = base;
             this.screen.setPixel(x, y, color);
         }
+
+        let dimensions: {small: Dimension, big: Dimension} = this.ppu.registers.oamselect.getObjectSizes();
+        let sprites: Sprite[] = this.ppu.sprites.getSprites();
+        let count: number = 0;
+        for (let sprite of sprites) {
+            let height: number = sprite.isBig() ? dimensions.big.height : dimensions.small.height;
+            let width: number = sprite.isBig() ? dimensions.big.width : dimensions.small.width;
+
+            let yStart: number = sprite.getYPosition();
+            let yEnd: number = sprite.getYPosition() + height;
+
+            if (yStart <= y && y <= yEnd) {
+                count++;
+                if (count == 31) break;
+                let tile: Tile = sprite.getTile();
+                let colors: Color[] = this.ppu.palette.getPalettesForBppType(sprite.getPaletteIndex(), BppType.Four);
+
+                for (let x: number = 0; x < width; x++) {
+                    let index: number = tile.data[y % height][x];
+                    if (index == 0) continue;
+                    let color: Color = colors[index];
+                    let xIndex: number = ((sprite.isXWrapped() ? 0x100 : 0x0) + sprite.getXPosition() + x) % 0x1FF;
+                    if (xIndex > Screen.WIDTH) continue;
+                    this.screen.setPixel(sprite.getXPosition() + x , y, color);
+                }
+            }
+        }
+
+
     }
 
 }
